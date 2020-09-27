7d80abdaf72c0556a7ee7cf6671d3f7895514206
Alberto Sotelo <irving.sotelo6543@alumnos.udg.mx>
2020-09-27 02:17:59 -0500

    Se agrego la opcion 1 y 2 asi como las funciones solicitadas

--- main.c
+++ main.c
@@ -1,8 +1,73 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void capturar_entero(){
+    int enteros [5];
+    float promedio;
+    int suma = 0;
+    for (size_t i = 0; i < 5; i++) //Captura enteros
+    {
+        scanf("%i", &enteros[i]);
+    }
+    
+    for (size_t i = 0; i < 5 ; i++) //Imprime enteros
+    {
+        printf("%i\n", enteros[i]);
+    }
+
+    for (size_t i = 0; i < 5 ; i++) //Calcula suma
+    {
+        suma = suma + enteros[i]; //suma += enteros[i]
+    }
+    printf("Suma: %i\n", suma); //Immprime suma
+    printf("Promedio: %f\n", suma/5.0); //Imprime promedios
+}
+
+void mostrar(int n, char cadena[]){
+    for (size_t i = 0; i < n; i++)
+    {
+        printf("%s", cadena);
+    }
+    
+}
 int main(){
-printf("Hola mundo\n");
+char op; 
+char cadena[20];
+int n;
+
+do
+{
+    printf("1. Capturar enteros\n");
+    printf("2. Mostrar cadena n veces\n");
+    printf("3. Agregar personaje\n");
+    printf("0. Salir\n");
+    scanf("%c", &op);
+
+    switch (op)
+    {
+    case '1':
+        capturar_entero();
+        break;
+    
+    case '2':
+        printf("Escribe una cadena de hasta 20 caracteres: \n");
+        fflush(stdin);
+        fgets(cadena, sizeof(cadena), stdin);
+        printf("n: ");
+        scanf("%i", &n);
+        mostrar (n, cadena);
+        break;
+
+    case '3':
+        break;    
+    default:
+
+        break;
+    }
+    
+    fflush(stdin);
+} while (op != '0');
+
 
 return 0;
 }
